45b14b7e3af0c40bd9004fc19f0daf78c587ca3a
Matt Powers <powers.matthew.douglas@gmail.com>
2021-10-25 15:48:44 -0700

    Completed user-can-create-entry feature.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
 /* global data */
 /* exported data */
+
+var $photoUrlInput = document.querySelector('#photoUrl');
+var $imgElement = document.querySelector('.image-submit');
+var $form = document.querySelector('form');
+var $title = document.querySelector('#title');
+var $textarea = document.querySelector('textarea');
+
+// -------Photo Upload Preview -----------
+
+function photoInputChangeHandler(event) {
+  var submittedImg = event.target.value;
+  $imgElement.setAttribute('src', submittedImg);
+}
+
+$photoUrlInput.addEventListener('change', photoInputChangeHandler);
+
+// --------Form inputs---------------------
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+  var formSubmissionData = {
+    id: data.nextEntryId,
+    title: $title.value,
+    notes: $textarea.value,
+    image: $photoUrlInput.value
+  };
+  data.nextEntryId++;
+  data.entries.unshift(formSubmissionData);
+  $imgElement.setAttribute('src', './images/placeholder-image-square.jpg');
+  $form.reset();
+}
+
+$form.addEventListener('submit', formSubmitHandler);
